a76ddbcc9917d9a761d8d6fdf067db3f41e36a33
Benjamin Würmli <benjamin.wuermli@bertschi.com>
2022-10-14 14:44:15 +0200

    almost

--- src/apps/truck/MVC/Views/car.js
+++ src/apps/truck/MVC/Views/car.js
@@ -14,23 +14,51 @@ class Car
         this.angle = 0;
         this.damaged = false;
 
-        this.sensor = new Sensor(this);
+        this.useBrain = (controlType === "AI");
+
+        if (controlType !== "DUMMY") {
+            this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([
+                this.sensor.rayCount,
+                6,
+                4
+            ]);
+        }
         this.controls = new Controls(controlType);
     }
 
-    update(roadBorders)
+    update(roadBorders, traffic)
     {
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        this.sensor.update(roadBorders);
+        if (this.sensor && !this.damaged) {
+            this.sensor.update(roadBorders, traffic);
+
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1-s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
+        }
     }
 
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders, traffic){
         for (let i = 0; i < roadBorders.length; i++) {
             if(polysIntersect(this.polygon, roadBorders[i])){
+                return true;
+            }
+        }
+        for (let i = 0; i < traffic.length; i++) {
+            if(polysIntersect(this.polygon, traffic[i].polygon)){
                 return true;
             }
         }
@@ -89,13 +117,13 @@ class Car
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx)
-    {
+    draw(ctx, color, showSensors = false){
         if (this.damaged) {
             ctx.fillStyle = 'red';
         } else {
-            ctx.fillStyle = 'yellow';
+            ctx.fillStyle = color;
         }
+
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
         for (let i = 1; i < this.polygon.length; i++) {
@@ -103,6 +131,8 @@ class Car
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if (this.sensor && showSensors) {
+            this.sensor.draw(ctx);
+        }
     }
 }
--- src/apps/truck/MVC/Views/sensor.js
+++ src/apps/truck/MVC/Views/sensor.js
@@ -3,25 +3,28 @@ class Sensor{
         this.car = car;
         this.rayCount = 5;
         this.rayLength = 110;
-        this.raySpread = Math.PI / 2;
+        this.raySpread = Math.PI * 0.6;
 
         this.rays = [];
         this.readings = [];
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         this.#castRays();
         this.readings = [];
-        for (let i = 0; i < this.rays.length; i++) {
+        for (let i = 0; i < this.rayCount; i++) {
             this.readings.push(
                 this.#getReading(this.rays[i],
-                    roadBorders)
+                    roadBorders,
+                    traffic
+                )
             );
         }
     }
 
-    #getReading(ray, roadBorders){
+    #getReading(ray, roadBorders, traffic){
         let touches = [];
+
         for (let i = 0; i < roadBorders.length; i++) {
             const touch = getIntersection(
                 ray[0],
@@ -33,6 +36,21 @@ class Sensor{
                 touches.push(touch);
             }
         }
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon;
+            for (let j = 0; j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
+                );
+                if (value) {
+                    touches.push(value);
+                }
+            }
+        }
+
         if (touches.length === 0) {
             return null;
         } else {
