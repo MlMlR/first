8712d34b26d1a77db0a204200ece600d69809695
Benjamin Würmli <benjamin.wuermli@bertschi.com>
2022-12-05 09:52:50 +0100

    advent

--- src/apps/AdventOfCode/MVC/AdventofCodeController.php
+++ src/apps/AdventOfCode/MVC/AdventofCodeController.php
@@ -9,14 +9,136 @@ class AdventofCodeController extends AbstractController
 
     public function showResult()
     {
-        $input = $this->day3b();
-//        $this->pageload("AdventOfCode", "advent", [
-//
-//        ]);
+        $this->old();
     }
 
+    public function old()
+    {
+        $draw = [38,54,68,93,72,12,33,8,98,88,21,91,53,61,26,36,18,80,73,47,3,5,55,92,67,52,25,40,56,95,9,62,30,31,85,65,14,2,78,75,15,39,87,27,58,42,60,32,41,83,51,77,10,66,70,4,37,6,89,23,16,49,48,63,94,97,86,64,74,82,7,0,11,71,44,43,50,69,45,81,20,28,46,79,90,34,35,96,99,59,1,76,22,24,17,57,13,19,84,29
+        ];
+        $input = file(__DIR__ . "/input_2021/day04.txt", FILE_IGNORE_NEW_LINES);
+        $boards = [];
+        $board = 0;
+        for ($i = 0; $i < count($input); $i++) {
+            if (empty($input[$i])){
+                $board++;
+            } else {
+                $z = explode(" ",str_replace("  ", " ", $input[$i]));
+                if ($z[0] == ""){
+                    array_shift($z);
+                }
+                $boards[$board][] = $z;
+            }
+
+        }
+        foreach( $boards as $board){
+            $this->drawBingoBoard($board);
+            echo "<br>";
+
+        }
+    }
+    public function drawBingoBoard($board)
+    {
+        foreach ($board as $row) {
+            foreach ($row as $field) {
+                echo $field . ", ";
+            }
+            echo "<br>";
+        }
+    }
+
+    public function day5(){
+
+//        [M]     [W] [M]
+//        [L] [Q] [S] [C] [R]
+//        [Q] [F] [F] [T] [N] [S]
+//        [N]     [V] [V] [H] [L] [J] [D]
+//        [D] [D] [W] [P] [G] [R] [D] [F]
+//        [T] [T] [M] [G] [G] [Q] [N] [W] [L]
+//        [Z] [H] [F] [J] [D] [Z] [S] [H] [Q]
+//        [B] [V] [B] [T] [W] [V] [Z] [Z] [M]
+//         1   2   3   4   5   6   7   8   9
+
+        $stack = array(
+            ["placeholder"],
+            ["B", "Z", "T"],
+            ["T", "H", "T", "D", "N"],
+            ["B", "F", "M", "D"],
+            ["T", "J", "G", "W", "V", "Q", "L"],
+            ["W", "D", "G", "P", "V", "F", "Q", "M"],
+            ["V", "Z", "Q", "G", "H", "F", "S"],
+            ["Z", "S", "N", "R", "L", "T", "C", "W"],
+            ["Z", "H", "W", "D", "J", "N", "R", "M"],
+            ["M", "Q", "L", "F", "D", "S"],
+            );
+
+        $input = file(__DIR__ . "/Input2022/day05_2022.txt", FILE_IGNORE_NEW_LINES);
+//        move 1 from 7 to 4
+        // not RTWZSGHFV
+
+        $r = 0;
+        foreach($stack as $pile){
+            foreach ($pile as $container){
+                echo $container . ", ";
+                $r++;
+            }
+            echo "<br>";
+        }
+        echo "<br>start moving<br><br>";
+
+
+        foreach ($input as $moves) {
+            $split = explode(" ", $moves);
+            $amount = $split[1];
+            $from = $split[3];
+            $to = $split[5];
+
+            echo "<br>move " . $amount . " containers from pile " . $from ." to pile " . $to . "<br>";
+
+            for ( $i = 0; $i < $amount; $i++){
+                $stack[0][] = array_pop($stack[$from]);
+            }
+            for ( $i = 0; $i < $amount; $i++){
+                $stack[$to][] = array_pop($stack[0]);
+            }
+
+            foreach($stack as $pile){
+                foreach ($pile as $container){
+                    echo $container . ", ";
+                    $t++;
+                }
+                echo "<br>";
+            }
+        }
+    }
+
+
+
+    public function day4(){
+        $input = file(__DIR__ . "/Input2022/day04_2022.txt", FILE_IGNORE_NEW_LINES);
+        $counter = 0;
+        $overlapingSquares = [];
+        for ( $i=0; $i<count($input); $i++){
+            $elves = explode( ",",$input[$i]);
+            $elve1Sqrs = explode("-", $elves[0]);
+            $elve2Sqrs = explode("-", $elves[1]);
+            $smal = max($elve1Sqrs[0],$elve2Sqrs[0]);
+            $big = min($elve1Sqrs[1],$elve2Sqrs[1]);
+
+            if (($big>=$smal)){
+           $counter++;
+            }
+        }
+        echo $counter;
+    }
+
+
+
     public function day3b(){
-        $input = file(__DIR__ . "/Input2022/day03_2022.txt", FILE_IGNORE_NEW_LINES);
+        $input = [3,3];
+//        $input = file(__DIR__ . "/Input2022/day03_2022.txt", FILE_IGNORE_NEW_LINES);
+
+
         $score = 0;
         $array0 = [];
         $array1 = [];
